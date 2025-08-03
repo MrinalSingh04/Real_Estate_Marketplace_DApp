efadf4c6b7896cf602dd7658dbe8895ab647dd66
Mrinal Singh <mrinalsingh7000@gmail.com>
2025-08-03 22:15:41 +0530

    contract-function-properties-done

--- web3/contracts/RealEstate.sol
+++ web3/contracts/RealEstate.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.9;
 
 contract RealEstate {
-    constructor() {}
+    //function in contract
+
+    function listProperty() external returns (uint256) {}
+
+    function updateProperty() external returns (uint256) {}
+
+    function buyProperty() external payable {}
+
+    function getAllProperties() public view returns (Property[] memory) {}
+
+    function getProperty() external view returns () {}
+
+    function getUserProperties() external view returns (Property[] memory) {}
 }
